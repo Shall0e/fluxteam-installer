ea41e3ebe86900513ff24b11db0fd42f4a9c178c
Shallow <100087924+Shall0e@users.noreply.github.com>
2024-08-31 01:09:08 -0400

    Add files via upload

---
+++ src/#COMPILE.cmd
@@ -0,0 +1,14 @@
+@echo off
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto :admin
+) else (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~0' -Verb runAs"
+    exit
+)
+
+:admin
+cd "C:\Users\Owner\Desktop\Fluxteam Installer\src"
+powershell.exe -Command "ps2exe -IconFile .\icon.ico -title 'FluxTeam Installer' -NoError -requireAdmin -inputFile .\main.ps1 -outputFile .\fluxteaminstaller.exe"
+pause
