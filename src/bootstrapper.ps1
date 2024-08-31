11358716bcbdb4158be5b35e9222fda88d231aff
Shallow <100087924+Shall0e@users.noreply.github.com>
2024-08-31 13:46:25 -0400

    Update bootstrapper.ps1

--- src/bootstrapper.ps1
+++ src/bootstrapper.ps1
@@ -109,7 +109,7 @@ logmessage -Type "FluxTeam" -Color Magenta -Message "Checking for updates..."
 Start-Sleep -Milliseconds 2000
 if ($outdated -and $installed) {
     Start-Sleep -Milliseconds 2000
-    powershellEmulator -On $false -Path "main.ps1"
+    powershellEmulator -On $false -Path "modules\update.ps1"
 }
 
 logmessage -Type "FluxTeam" -Color Magenta -Message "Starting FluxTeam..."
